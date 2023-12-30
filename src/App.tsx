53efe18ad72731a40aa7213e1427a20d103856b9
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-30 07:46:47 +0100

    setup editable span

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useState, useRef, useEffect, FormEvent } from "react";
 import "./App.css";
 
 export function App() {
+  const [text, setText] = useState<string>("Vegeta's playground");
+  const positionRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    if (positionRef.current) {
+      positionRef.current.textContent = text;
+    }
+  }, []);
+
+  const handleInput = (event: FormEvent<HTMLSpanElement>) => {
+    const newText = event.currentTarget.textContent || "";
+    console.log("New text:", newText);
+    setText(newText);
+  };
+
   return (
     <main>
       <h1>Vegeta's Playground</h1>
-      <div>More content to come</div>
+      <div className="content">
+        <span
+          contentEditable
+          className="editable-text"
+          ref={positionRef}
+          onInput={handleInput}
+        />
+      </div>
     </main>
   );
 }
