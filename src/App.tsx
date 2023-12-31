6313b1b1e9dfc3de51a1760be71b5e6cdeff8ffd
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-31 06:19:35 +0100

    request suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import debounce from "lodash.debounce";
 import "./App.css";
 
 export function App() {
-  const [text, setText] = useState("Vegeta's playground");
+  const [text, setText] = useState("Naruto is amazing.");
   const [suggestionIndex, setSuggestionIndex] = useState(0);
   const [shouldShowSuggestion, setShouldShowSuggestion] = useState(false);
   const [suggestions, setSuggestions] = useState<string[]>([]);
@@ -42,7 +42,9 @@ export function App() {
       if (hasUserStoppedTyping) {
         // Fetch suggestions from GPT 4 API
 
-        const prompt = `This is my current text: "${text}"\n\nI want you to give me 5 suggestions to continue this text. Each suggestion should be two sentences. If the current text is not a completed sentence, then each suggestion should complete the sentence and then add 2 more sentences. So all in all, we would have 5 suggestions, each with 2 sentences or 2 sentences and completing the current text if the last sentence is incomplete.Enter your suggestions like:\n\n1. "first suggestion" 2. "second suggestion" 3. "third suggestion" 4. "fourth suggestion" 5. "fifth suggestion"\n\n`;
+        console.log(latestTextRef.current);
+
+        const prompt = `I'm building a text editor with AI auto completion.\n\nThis is my current text: "${latestTextRef.current}"\n\nI want you to give me 3 suggestions to continue this text. Each suggestion should be two sentences. Do not include the existing text (it's unnecessary), your goal is to add suggestions of how the text continues. If the current text is not a completed sentence (not ending with punctuation that defines end of sentence e.g. question mark), then each suggestion should complete the sentence and then add 2 more sentences.\n\nFor example, if the text is "Naruto is great, because", then you would complete it by giving me " he never gives up". In addition to completing it, you would add two new sentences.So it would end up being " he never gives up. {two more sentences}".\n\nIf the text had been "Naruto is amazing.", we know it is a full sentence, so you just simply give me two sentences for each suggestion.\n\nTo recap: we would have 3 suggestions, each with 2 sentences or completing the current text if sentence is incomplete, then add 2 sentences. Enter your suggestions like:\n\n>first suggestion>second suggestion>third suggestion\n\n`;
 
         const payload = {
           model: "gpt-4-1106-preview",
@@ -52,10 +54,10 @@ export function App() {
               content: prompt,
             },
           ],
-          temperature: 0.5,
-          frequency_penalty: 0.5,
-          presence_penalty: 0.5,
-          max_tokens: 4000,
+          temperature: 0.5, // how creative suggestions are
+          frequency_penalty: 0.2, // how repetitive words are
+          presence_penalty: 0.2, // how repetitive topics are
+          max_tokens: 300,
         };
 
         try {
@@ -65,24 +67,24 @@ export function App() {
             {
               headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${import.meta.env.VITE_OPEN_API_KEY}`,
+                Authorization: `Bearer ${import.meta.env.VITE_OPENAI_KEY}`,
               },
               method: "POST",
               body: JSON.stringify(payload),
             }
           );
 
-          console.log(response);
+          console.log("response", await response.json());
+          setSuggestionIndex(0);
+          setShouldShowSuggestion(true);
         } catch (error) {
           console.error(error);
+          throw error;
         }
-
-        setSuggestionIndex(0);
-        setShouldShowSuggestion(true);
       } else {
         setShouldShowSuggestion(false);
       }
-    }, 800),
+    }, 1000),
     []
   );
 
